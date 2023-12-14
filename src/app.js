f02ee81ed8b0b0009c7fa7412997df7cdd99ac6f
Eduardo Souza <103686965+DevEduardoSouza@users.noreply.github.com>
2023-12-14 15:14:31 -0300

    feat: add conexão ao banco de dados

--- src/app.js
+++ src/app.js
@@ -1,8 +1,15 @@
 const express = require("express");
+require("dotenv").config();
 const app = express();
+
+const port = process.env.PORT || 8000;
 
 app.use(express.json());
 
+// DB connection
+const connection = require("../src/db/conn");
+connection();
+
 app.listen(3000, () => {
-  console.log("Servidor online");
+  console.log(`Servidor online na porta ${port}!`);
 });
---
+++ src/db/conn.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+async function main() {
+  try {
+    await mongoose.connect(`mongodb+srv://${process.env.USER}:${process.env.PASSWORD}@cluster0.qyn8qvx.mongodb.net/?retryWrites=true&w=majority`)
+    console.log("Conexão ao banco feita com sucesso");
+  } catch (error) {
+    console.log("Erro ao conectar ao banco", error);
+  }
+}
+
+module.exports = main;
