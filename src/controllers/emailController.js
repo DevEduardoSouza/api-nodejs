1f007dbac4a2c5d39767af6c984cc24d9e108209
Eduardo Souza <103686965+DevEduardoSouza@users.noreply.github.com>
2023-12-14 22:06:47 -0300

    feat: add métodos getAll e getById

--- src/controllers/emailController.js
+++ src/controllers/emailController.js
@@ -1,6 +1,5 @@
 const Email = require("../models/Email");
 
-
 const emailController = {
   create: async (req, res) => {
     try {
@@ -8,17 +7,29 @@ const emailController = {
         email: req.body.email,
         hashedPassword: req.body.hashedPassword,
       };
-      console.log("Opa");
-  
-      const response = await Email.create(email)
-        .then((resultado) => {
-          console.log("Registro criado com sucesso:", resultado);
-        })
-        .catch((error) => console.log("Erro ao criar registro:", error));
+
+      const response = await Email.create(email);
 
       res.status(201).json({ response, msg: "Email criado com sucesso!" });
     } catch (error) {
       console.log(`Error ao criar um email: ${error}`);
+    }
+  },
+  getAll: async (req, res) => {
+    try {
+      const emails = await Email.find();
+      res.json(emails);
+    } catch (error) {
+      console.log(`Error ao buscar todos os emails: ${error}`);
+    }
+  },
+  getById: async (req, res) => {
+    try {
+      const id = req.params.id;
+      const emails = await Email.findById(id);
+      res.json(emails);
+    } catch (error) {
+      console.log(`Error ao buscar o email: ${error}`);
     }
   },
 };
--- src/routes/email.js
+++ src/routes/email.js
@@ -2,9 +2,15 @@ const router = require("express").Router();
 const emailController = require("../controllers/emailController");
 
 router.route("/email").post((req, res) => {
-  console.log("Chegou na rota POST /api/email");
-  console.log(req.body);
   emailController.create(req, res);
+});
+
+router.route("/email").get((req, res) => {
+  emailController.getAll(req, res);
+});
+
+router.route("/email/:id").get((req, res) => {
+  emailController.getById(req, res);
 });
 
 module.exports = router;
